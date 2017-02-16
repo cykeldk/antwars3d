3159f4609447cccf7f273fd4addf55f94113f6e6
Jonathan szigethy <Jonathan szigethy>
2017-02-16 11:59:26 +0100

    assets

---
+++ Ant Wars 3D/Assets/PlayVideo.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+[RequireComponent (typeof(AudioSource))]
+
+public class BackgroundVideo : MonoBehaviour
+{
+    public MovieTexture movie;
+    private AudioSource audio;
+
+    void Start()
+    {
+        GetComponent<RawImage>().texture = movie as MovieTexture;
+        audio = GetComponent<AudioSource>();
+        audio.clip = movie.audioClip;
+        movie.Play();
+        audio.Play();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && movie.isPlaying)
+        {
+            movie.Pause();
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space) && !movie.isPlaying)
+        {
+            movie.Play();
+        }
+
+
+    }
+}
